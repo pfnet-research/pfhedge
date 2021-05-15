4727224271ea47c1f4998515eac88fd61cb302ef
Shota Imaki <shota.imaki.0801@gmail.com>
2021-05-15 12:40:33 +0900

    CHORE: Create Makefile

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -eu
-
-python3 -m pytest --doctest-modules pfhedge
-python3 -m pytest --doctest-modules tests
-
-python3 -m black --quiet .
-python3 -m isort --force-single-line-imports --quiet .
