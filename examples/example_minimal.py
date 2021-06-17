798b914a113ce03f357fecc37d0e556af8e5840e
simaki <shota.imaki.0801@gmail.com>
2021-06-17 10:18:10 +0900

    DOC: Fix minimal example (close #68) (#75)
    * DOC: Fix minimal example (close #68)

---
+++ examples/example_minimal.py
@@ -0,0 +1,24 @@
+# Here is a minimal example to try out Deep Hedging.
+
+import sys
+
+sys.path.append("..")
+
+if __name__ == "__main__":
+    from pfhedge import Hedger
+    from pfhedge.instruments import BrownianStock
+    from pfhedge.instruments import EuropeanOption
+    from pfhedge.nn import MultiLayerPerceptron
+
+    # Prepare a derivative to hedge
+    deriv = EuropeanOption(BrownianStock(cost=1e-4))
+
+    # Create your hedger
+    model = MultiLayerPerceptron()
+    hedger = Hedger(
+        model, inputs=["log_moneyness", "expiry_time", "volatility", "prev_hedge"]
+    )
+
+    # Fit and price
+    hedger.fit(deriv)
+    price = hedger.price(deriv)
