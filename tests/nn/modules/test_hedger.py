6917ff00cb2b46e89eda676b236fa21415e28f93
Shota Imaki <shota.imaki.0801@gmail.com>
2021-08-20 23:08:38 +0900

    TEST: Add tests

--- tests/nn/modules/test_hedger.py
+++ tests/nn/modules/test_hedger.py
@@ -9,6 +9,7 @@ from torch.nn import Module
 from torch.testing import assert_close
 
 from pfhedge.instruments import BrownianStock
+from pfhedge.instruments import Derivative
 from pfhedge.instruments import EuropeanOption
 from pfhedge.nn import BlackScholes
 from pfhedge.nn import EntropicRiskMeasure
@@ -20,6 +21,16 @@ from pfhedge.nn import WhalleyWilmott
 
 def void(*args, **kwargs):
     pass
+
+
+class ZeroDerivative(Derivative):
+    def __init__(self, underlier, maturity=20 / 250):
+        super().__init__()
+        self.underlier = underlier
+        self.maturity = maturity
+
+    def payoff(self):
+        return torch.zeros_like(self.ul().spot[..., 0])
 
 
 class FakeModule(Module):
@@ -84,7 +95,7 @@ class TestHedger:
         torch.manual_seed(42)
         N, T = 10, 20
 
-        derivative = EuropeanOption(BrownianStock())
+        derivative = ZeroDerivative(BrownianStock())
         output = torch.randn(N, T - 1)
 
         m = FakeModule(output)
@@ -96,7 +107,28 @@ class TestHedger:
             # so that the simulation is not performed
             result = hedger.compute_pnl(derivative)
 
-        expect = (spot.diff(dim=-1) * output).sum(-1) - derivative.payoff()
+        expect = (spot.diff(dim=-1) * output).sum(-1)
+        assert_close(result, expect)
+
+    def test_compute_pnl_payoff(self):
+        N, T = 10, 20
+        derivative0 = EuropeanOption(BrownianStock())
+        derivative1 = ZeroDerivative(BrownianStock())
+        output = torch.randn(N, T - 1)
+
+        hedger0 = Hedger(FakeModule(output), ["empty"])
+        hedger1 = Hedger(FakeModule(output), ["empty"])
+
+        spot = torch.randn(N, T).exp()
+        derivative0.ul().register_buffer("spot", spot)
+        derivative1.ul().register_buffer("spot", spot)
+        with patch("pfhedge.instruments.BrownianStock.simulate", void):
+            # so that the simulation is not performed
+            pnl0 = hedger0.compute_pnl(derivative0)
+            pnl1 = hedger1.compute_pnl(derivative1)
+
+        result = pnl0 - pnl1
+        expect = -derivative0.payoff()
         assert_close(result, expect)
 
     def test_compute_pnl_cost(self):
