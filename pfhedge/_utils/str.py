a8bfdc8c2bf18b9488a8bd44db591f213221895a
simaki <shota.imaki.0801@gmail.com>
2021-08-20 17:08:42 +0900

    Update pfhedge/_utils/str.py

--- pfhedge/_utils/str.py
+++ pfhedge/_utils/str.py
@@ -10,7 +10,7 @@ def _format_float(value: float) -> str:
     >>> _format_float(1e-4)
     '1.0000e-04'
     """
-    # format a float according to pytorch printoptions
+    # format a float following PyTorch printoptions
     # see `torch.set_printoptions` for details
     tensor = torch.tensor([value])
     return torch._tensor_str._Formatter(tensor).format(value)
