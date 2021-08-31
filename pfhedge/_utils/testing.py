581d0edacde62694462625c286fc7894a021329a
simaki <shota.imaki.0801@gmail.com>
2021-08-31 17:52:38 +0900

    TEST: Add assert monotone, convex, cash-invariant (close #294) (#296)

---
+++ pfhedge/_utils/testing.py
@@ -0,0 +1,78 @@
+from typing import Callable
+
+from torch import Tensor
+from torch.testing import assert_allclose
+
+
+def assert_monotone(
+    fn: Callable[[Tensor], Tensor],
+    x1: Tensor,
+    x2: Tensor,
+    increasing: bool = False,
+    allow_equal: bool = False,
+) -> None:
+    """Assert fn is monotone function for :math:`x_1 > x_2`.
+
+    .. math::
+
+        f(x_1) > f(x_2)
+
+    Args:
+        fn (callable[[torch.Tensor], torch.Tensor]): Function to test.
+        x1 (torch.Tensor): First tensor.
+        x2 (torch.Tensor): Second tensor.
+        increasing (bool, default=True): Whether to test increasing monotonicity.
+            Only False is allowed.
+        allow_equal (bool, default=False): Whether to allow :math:`x_1 = x_2`.
+            Only False is allowed.
+
+    Returns:
+        None
+
+    Raises:
+        AssertionError: If fn is not monotone.
+    """
+    assert not increasing, "not supported"
+    assert not allow_equal, "not supported"
+    assert (x1 > x2).all(), "x1 > x2 should be satisfied"
+
+    assert (fn(x1) < fn(x2)).all()
+
+
+def assert_convex(
+    fn: Callable[[Tensor], Tensor], x1: Tensor, x2: Tensor, alpha: float
+) -> None:
+    """Assert convexity.
+
+    .. math::
+
+        f(alpha * x_1 + (1 - alpha) * x_2) <=
+        alpha * f(x_1) + (1 - alpha) * f(x_2)
+
+    Args:
+        fn (callable[[torch.Tensor], torch.Tensor]): Function to test.
+        x1 (torch.Tensor): First tensor.
+        x2 (torch.Tensor): Second tensor.
+        alpha (float): The parameter alpha.
+    """
+    y = fn(alpha * x1 + (1 - alpha) * x2)
+    y1 = fn(x1)
+    y2 = fn(x2)
+    assert y <= alpha * y1 + (1 - alpha) * y2
+
+
+def assert_cash_invariant(
+    fn: Callable[[Tensor], Tensor], x: Tensor, c: float, **kwargs
+) -> None:
+    """Assert cash invariance.
+
+    .. math::
+
+        f(x + c) = f(x) - c
+
+    Args:
+        fn (callable): Function to test cash invariance.
+        x (float): The input tensor.
+        c (float): The parameter c.
+    """
+    assert_allclose(fn(x + c), fn(x) - c, **kwargs)
