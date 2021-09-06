ae030b8ebb503c1bbaf339066816e8dd9cc1411f
Shota Imaki <shota.imaki.0801@gmail.com>
2021-09-06 16:22:01 +0900

    fix

--- pfhedge/features/features.py
+++ pfhedge/features/features.py
@@ -76,7 +76,7 @@ class Variance(StateIndependentFeature):
     def __str__(self) -> str:
         return "variance"
 
-    def __getitem__(self, time_step: Optional[int]) -> Tensor:
+    def get(self, time_step: Optional[int]) -> Tensor:
         index = [time_step] if isinstance(time_step, int) else ...
         return self.derivative.ul().variance[:, index].unsqueeze(-1)
 
