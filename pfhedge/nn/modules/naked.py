18a56801bb770598c331bd9f47910c2f7c51f169
Shota Imaki <shota.imaki.0801@gmail.com>
2021-09-06 19:22:54 +0900

    MAINT: Simplify Naked

--- pfhedge/nn/modules/naked.py
+++ pfhedge/nn/modules/naked.py
@@ -33,6 +33,5 @@ class Naked(Module):
         self.out_features = out_features
 
     def forward(self, input: Tensor) -> Tensor:
-        return torch.cat(
-            [torch.zeros_like(input[..., :1]) for _ in range(self.out_features)], -1
-        )
+        size = input.size()[:-1] + (self.out_features,)
+        return torch.zeros(size, dtype=input.dtype, device=input.device)
