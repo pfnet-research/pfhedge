bc05fea7852c455697f4af94fe962606f5c82282
Masanori HIRANO <masa.hirano.1996@gmail.com>
2021-12-21 11:13:58 +0900

    added test

--- tests/test_autogreek.py
+++ tests/test_autogreek.py
@@ -21,3 +21,15 @@ def test_vega():
     result = autogreek.vega(pricer, variance=variance, coef=coef)
     expect = 2 * coef * variance.sqrt()
     assert_close(result, expect)
+
+
+def test_theta():
+    def pricer(time_to_maturity, coef):
+        return coef * time_to_maturity.pow(2)
+
+    torch.manual_seed(42)
+    time_to_maturity = torch.randn(10).exp()  # make it positive
+    coef = torch.randn(10)
+    result = autogreek.theta(pricer, time_to_maturity=time_to_maturity, coef=coef)
+    expect = -2 * coef * time_to_maturity
+    assert_close(result, expect)
