6dd31f2bf6cc654e1258ae7a55e54dc1e24c5e22
simaki <shota.imaki.0801@gmail.com>
2022-02-04 17:45:50 +0900

    WIP: ENH: Add antithetic sampling of randn (close #449) (#450)

--- pfhedge/stochastic/__init__.py
+++ pfhedge/stochastic/__init__.py
@@ -2,3 +2,4 @@ from .brownian import generate_brownian
 from .brownian import generate_geometric_brownian
 from .cir import generate_cir
 from .heston import generate_heston
+from .random import randn_antithetic
