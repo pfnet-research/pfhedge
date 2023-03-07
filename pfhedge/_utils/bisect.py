d85ce440f0db710e4233619e5ad1699549c720f2
Masanori HIRANO <masanorihirano@users.noreply.github.com>
2023-03-07 18:34:23 +0900

    Update pfhedge/_utils/bisect.py
    Co-authored-by: Kentaro IMAJO <173752+imos@users.noreply.github.com>

--- pfhedge/_utils/bisect.py
+++ pfhedge/_utils/bisect.py
@@ -26,9 +26,9 @@ def bisect(
         fn (callable[[Tensor], Tensor]): A monotone function.
         target (Tensor): Target of function values.
         lower (Tensor or float): Lower bound of binary search.
-                                 (If cuda Tensor is required for the input of fn, cuda Tensor is necessaary.)
+                                 This must be a CUDA Tensor if fn expects to input a CUDA Tensor.
         upper (Tensor or float): Upper bound of binary search.
-                                 (If cuda Tensor is required for the input of fn, cuda Tensor is necessaary.)
+                                 This must be a CUDA Tensor if fn expects to input a CUDA Tensor.
         precision (float, default=1e-6): Precision of output.
         max_iter (int, default 100000): If the number of iterations exceeds this
             value, abort computation and raise RuntimeError.
