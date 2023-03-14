5940902b63314442a82719e0464ee67d9cae5d29
Masanori HIRANO <masa.hirano.1996@gmail.com>
2023-03-14 11:02:24 +0900

    update flex ci for avoiding docker image push

--- .flexci/linux/test.sh
+++ .flexci/linux/test.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 set -uex
+
+git clone https://github.com/pyenv/pyenv.git /opt/pyenv
+PYENV_ROOT=/opt/pyenv
+PATH=${PYENV_ROOT}/shims:${PYENV_ROOT}/bin:${PATH}
+
+# Install Python.
+if [ -z $python_version ]; then
+  python_version="3.9.7"
+fi
+echo "python ${python_version}"
+pyenv install ${python_version} && \
+pyenv global ${python_version}
+
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+PATH=${HOME}/.poetry/env:${PATH}
+
 poetry install
 
 pip list
