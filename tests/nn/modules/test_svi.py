726b4773fd215f03afe418a6c255a828c0e6e874
Masanori HIRANO <masa.hirano.1996@gmail.com>
2023-03-28 15:42:54 +0900

    remove assert_allclose and use assert_close

--- tests/nn/modules/test_svi.py
+++ tests/nn/modules/test_svi.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 from torch.nn.functional import relu
-from torch.testing import assert_allclose
+from torch.testing import assert_close
 
 from pfhedge.nn import SVIVariance
 
@@ -13,20 +13,20 @@ def test_svi(device: str = "cpu"):
     m = SVIVariance(a=1.0, b=0, rho=0.1, m=0.2, sigma=0.3).to(device)
     result = m(input)
     expect = torch.full_like(result, m.a)
-    assert_allclose(result, expect)
+    assert_close(result, expect)
 
     # for sigma = 0 and rho = -1, output = a + 2 b relu(x)
     m = SVIVariance(a=1.0, b=0, rho=-1, m=0.0, sigma=0.3).to(device)
     result = m(input)
     expect = m.a + 2 * m.b * relu(input - m.m)
-    assert_allclose(result, expect)
+    assert_close(result, expect)
 
     # m translates
     m0 = SVIVariance(a=0.1, b=0.2, rho=0.3, m=0.0, sigma=0.5).to(device)
     m1 = SVIVariance(a=0.1, b=0.2, rho=0.3, m=0.4, sigma=0.5).to(device)
     result = m0(input)
     expect = m1(input + m1.m)
-    assert_allclose(result, expect)
+    assert_close(result, expect)
 
 
 @pytest.mark.gpu
