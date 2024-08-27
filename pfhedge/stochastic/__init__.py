17bf68ae93251a3e64fdbe093f65ff883dcd05c9
rhandal-pfn <rhandal@preferred.jp>
2024-08-27 17:10:23 +0900

    kou's jump model

--- pfhedge/stochastic/__init__.py
+++ pfhedge/stochastic/__init__.py
@@ -2,6 +2,7 @@ from .brownian import generate_brownian  # NOQA
 from .brownian import generate_geometric_brownian  # NOQA
 from .cir import generate_cir  # NOQA
 from .heston import generate_heston  # NOQA
+from .kou_jump import generate_kou_jump  # noqa: F401
 from .local_volatility import generate_local_volatility_process  # NOQA
 from .merton_jump import generate_merton_jump  # NOQA
 from .random import randn_antithetic  # NOQA
