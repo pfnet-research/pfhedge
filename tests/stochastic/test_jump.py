308f95051b15fbbe82ddb3d521c36dd44e80e1c0
rhandal-pfn <rhandal@preferred.jp>
2024-08-28 14:12:29 +0900

    lint fix

--- tests/stochastic/test_jump.py
+++ tests/stochastic/test_jump.py
@@ -32,11 +32,9 @@ class TestGenerateJumpStock:
         std = 0.2 * sqrt(1 / n_paths)
         assert_close(result, expect, atol=3 * std, rtol=0)
 
-
     @pytest.mark.gpu
     def test_generate_brownian_mean_no_jump_gpu(self):
         self.test_generate_brownian_mean_no_jump(device="cuda")
-
 
     def test_generate_brownian_mean_no_jump1(self, device: str = "cpu"):
         torch.manual_seed(42)
@@ -50,11 +48,9 @@ class TestGenerateJumpStock:
         std = 0.2 * sqrt(1 / n_paths)
         assert_close(result, expect, atol=3 * std, rtol=0)
 
-
     @pytest.mark.gpu
     def test_generate_brownian_mean_no_jump1_gpu(self):
         self.test_generate_brownian_mean_no_jump1(device="cuda")
-
 
     def test_generate_brownian_mean_no_jump_std(self, device: str = "cpu"):
         # kou jump has no std
@@ -78,11 +74,9 @@ class TestGenerateJumpStock:
         std = 0.4 * sqrt(1 / n_paths)
         assert_close(result, expect, atol=3 * std, rtol=0)
 
-
     @pytest.mark.gpu
     def test_generate_brownian_mean_no_jump_std_gpu(self):
         self.test_generate_brownian_mean_no_jump_std(device="cuda")
-
 
     def test_generate_brownian_mean(self, device: str = "cpu"):
         torch.manual_seed(42)
@@ -98,11 +92,9 @@ class TestGenerateJumpStock:
         std = 0.2 * sqrt(1 / n_paths) + 0.3 * sqrt(1 / n_paths)
         assert_close(result, expect, atol=3 * std, rtol=0)
 
-
     @pytest.mark.gpu
     def test_generate_brownian_mean_gpu(self):
         self.test_generate_brownian_mean(device="cuda")
-
 
     def test_generate_jump_nosigma(self, device: str = "cpu"):
         torch.manual_seed(42)
@@ -117,16 +109,22 @@ class TestGenerateJumpStock:
         mu = 0.1
         dt = 0.01
         result = self.jump_test_func(
-            1, n_steps, mu=mu, sigma=0, dt=dt, jump_per_year=0, device=torch.device(device)
+            1,
+            n_steps,
+            mu=mu,
+            sigma=0,
+            dt=dt,
+            jump_per_year=0,
+            device=torch.device(device),
         ).log()
-        expect = torch.linspace(0, mu * dt * (n_steps - 1), n_steps).unsqueeze(0).to(device)
+        expect = (
+            torch.linspace(0, mu * dt * (n_steps - 1), n_steps).unsqueeze(0).to(device)
+        )
         assert_close(result, expect)
-
 
     @pytest.mark.gpu
     def test_generate_jump_nosigma_gpu(self):
         self.test_generate_jump_nosigma(device="cpu")
-
 
     def test_generate_jump_nosigma2(self, device: str = "cpu"):
         # kou jump has no std
@@ -146,14 +144,14 @@ class TestGenerateJumpStock:
         result = self.jump_test_func(
             1, n_steps, mu=mu, sigma=0, dt=dt, jump_std=0, device=torch.device(device)
         ).log()
-        expect = torch.linspace(0, mu * dt * (n_steps - 1), n_steps).unsqueeze(0).to(device)
+        expect = (
+            torch.linspace(0, mu * dt * (n_steps - 1), n_steps).unsqueeze(0).to(device)
+        )
         assert_close(result, expect)
-
 
     @pytest.mark.gpu
     def test_generate_jump_nosigma2_gpu(self):
         self.test_generate_jump_nosigma2(device="cuda")
-
 
     def test_generate_jump_std(self, device: str = "cpu"):
         torch.manual_seed(42)
@@ -168,11 +166,9 @@ class TestGenerateJumpStock:
         expect = torch.full_like(result, 0.2)
         assert_close(result, expect, atol=0, rtol=0.1)
 
-
     @pytest.mark.gpu
     def test_generate_jump_std_gpu(self):
         self.test_generate_jump_std(device="cuda")
-
 
     def test_generate_jump_std2(self, device: str = "cpu"):
         # kou jump has no std
@@ -190,11 +186,9 @@ class TestGenerateJumpStock:
         expect = torch.full_like(result, 0.2)
         assert_close(result, expect, atol=0, rtol=0.1)
 
-
     @pytest.mark.gpu
     def test_generate_jump_std2_gpu(self):
         self.test_generate_jump_std2(device="cuda")
-
 
     def test_generate_jump_mean_init_state(self, device: str = "cpu"):
         torch.manual_seed(42)
@@ -202,7 +196,11 @@ class TestGenerateJumpStock:
         n_steps = 250
 
         output = self.jump_test_func(
-            n_paths, n_steps, init_state=1.0, jump_per_year=0, device=torch.device(device)
+            n_paths,
+            n_steps,
+            init_state=1.0,
+            jump_per_year=0,
+            device=torch.device(device)
         )
         assert output.size() == torch.Size((n_paths, n_steps))
         result = output[:, -1].mean()
@@ -236,11 +234,9 @@ class TestGenerateJumpStock:
         std = 0.2 * sqrt(1 / n_paths)
         assert_close(result, expect, atol=3 * std, rtol=0)
 
-
     @pytest.mark.gpu
     def test_generate_jump_mean_init_state_gpu(self):
         self.test_generate_jump_mean_init_state(device="cuda")
-
 
     def test_generate_jump_mean_mu(self, device: str = "cpu"):
         torch.manual_seed(42)
@@ -257,11 +253,9 @@ class TestGenerateJumpStock:
         std = 0.2 * sqrt(1 / n_paths)
         assert_close(result, expect, atol=3 * std, rtol=0)
 
-
     @pytest.mark.gpu
     def test_generate_jump_mean_mu_gpu(self):
         self.test_generate_jump_mean_mu(device="cuda")
-
 
     def test_generate_jump_dtype(self, device: str = "cpu"):
         torch.manual_seed(42)
@@ -276,11 +270,9 @@ class TestGenerateJumpStock:
         )
         assert output.dtype == torch.float64
 
-
     @pytest.mark.gpu
     def test_generate_jump_dtype_gpu(self):
         self.test_generate_jump_dtype(device="cuda")
-
 
     def test_generate_jump_sobol_mean(self, device: str = "cpu"):
         n_paths = 10000
@@ -288,14 +280,17 @@ class TestGenerateJumpStock:
 
         engine = RandnSobolBoxMuller(seed=42, scramble=True)
         output = self.jump_test_func(
-            n_paths, n_steps, engine=engine, jump_per_year=0, device=torch.device(device)
+            n_paths,
+            n_steps,
+            engine=engine,
+            jump_per_year=0,
+            device=torch.device(device)
         )
         assert output.size() == torch.Size((n_paths, n_steps))
         result = output[:, -1].mean()
         expect = torch.ones_like(result).to(device)
         std = 0.2 * sqrt(1 / n_paths)
         assert_close(result, expect, atol=10 * std, rtol=0)
-
 
     @pytest.mark.gpu
     def test_generate_jump_sobol_mean_gpu(self):
