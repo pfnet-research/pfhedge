0bac4728950bfdf6ccf6f2128347d523bea9e86f
rhandal-pfn <rhandal@preferred.jp>
2024-08-30 12:33:16 +0900

    full coverage test

--- tests/instruments/primary/test_merton_jump.py
+++ tests/instruments/primary/test_merton_jump.py
@@ -18,6 +18,7 @@ class TestMertonJumpStock:
         s.simulate(n_paths=1000)
 
         assert not s.variance.isnan().any()
+        assert not s.volatility.isnan().any()
 
     @pytest.mark.gpu
     @pytest.mark.parametrize("seed", range(1))
@@ -35,11 +36,13 @@ mu=0., sigma=0.2000, jump_per_year=68, jump_mean=0., jump_std=0.0100, cost=1.000
         s.simulate(time_horizon=0.2, n_paths=10)
         assert s.spot.size() == torch.Size((10, 3))
         assert s.variance.size() == torch.Size((10, 3))
+        assert s.volatility.size() == torch.Size((10, 3))
 
         s = self.jump_test_class(dt=0.1).to(device)
         s.simulate(time_horizon=0.25, n_paths=10)
         assert s.spot.size() == torch.Size((10, 4))
         assert s.variance.size() == torch.Size((10, 4))
+        assert s.volatility.size() == torch.Size((10, 4))
 
     @pytest.mark.gpu
     def test_simulate_shape_gpu(self):
